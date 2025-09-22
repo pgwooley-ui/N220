8803c51b2a08b650549c569b55db1fb8e48516df
pgwooley-ui <pgwooley@iu.edu>
2025-09-22 09:44:56 -0400

    Add files via upload

---
+++ j.js
@@ -0,0 +1,76 @@
+console.log("file");
+
+let tickets = 3;
+console.log("Tickets", tickets);
+
+let cost = 14;
+console.log("Cost", cost);
+
+let total = (cost*tickets);
+console.log("Total", total);
+
+//
+
+let account = 235.87;
+console.log("Account", account);
+
+let shirt = 35;
+console.log("Shirt", shirt);
+
+let pants = 75;
+console.log("Pants", pants);
+
+let shoes = 60;
+console.log("Shoes", shoes);
+
+let outfit = (shirt+pants+shoes);
+console.log("Outfit", outfit);
+
+let balance = (account-outfit);
+console.log("Balance", balance);
+
+//boolean
+
+let isTrue = true;
+let isFalse = false;
+
+console.log(isTrue, isFalse);
+
+let addJacket = false;
+console.log("Add Jacket = ",addJacket);
+
+//
+
+let pizza = 4;
+console.log("Pizza", pizza);
+
+let slices = 8 * pizza;
+console.log("Slices", slices);
+
+let student = 2.5;
+console.log("Student", student);
+
+let remainder = slices % student;
+
+let eaten = (slices/student * student) -remainder;
+console.log("Eaten", eaten);
+
+let students_fed = (eaten/student);
+console.log("Students Fed", students_fed);
+
+let left = remainder;
+console.log("Left",left);
+
+let adult = 12;
+let child = 6;
+let drink = 1.50;
+
+let price = (adult + adult + child +drink +drink +drink);
+console.log("Price",price);
+
+let week1 = 202.45;
+let week2 = 134.97;
+let week3 = 256.63;
+let week4 = 178.22;
+let tips = ((week1 + week2 + week3 + week4)/4);
+console.log("tips",tips);
